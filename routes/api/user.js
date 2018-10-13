2b7abc9d1feee12be19ce3c3e066ea709e16b76d
Jian Li <Jian.Li-4@student.uts.edu.au>
2018-10-13 20:50:41 +1100

    Update error handling

--- routes/api/user.js
+++ routes/api/user.js
@@ -8,68 +8,80 @@ const User = require("../../models/user");
 // @route   POST api/user
 // @desc    Sign up a new user
 router.post("/signup", (req, res) => {
+
   User.find({ email: req.body.email })
     .exec()
     .then(users => {
+
       if (users.length >= 1) {
         return res.status(409).json({
           message: "This email is already registered!"
         });
-      } else {
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-          if (err) {
-            return res.status(500).json({
+      }
+
+      bcrypt.hash(req.body.password, 10, (err, hash) => {
+
+        if (err) {
+          return res.status(500).json({
+            error: err
+          });
+        }
+
+        const user = new User({
+          _id: new mongoose.Types.ObjectId(),
+          email: req.body.email,
+          password: hash,
+          teams: req.body.teams
+        });
+
+        user
+          .save()
+          .then(() => {
+            res.status(201).json({
+              message: "New user created!"
+            });
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(500).json({
               error: err
             });
-          } else {
-            const user = new User({
-              _id: new mongoose.Types.ObjectId(),
-              email: req.body.email,
-              password: hash,
-              teams: req.body.teams
-            });
-            user
-              .save()
-              .then(() => {
-                res.status(201).json({
-                  message: "New user created!"
-                });
-              })
-              .catch(err => {
-                console.log(err);
-                res.status(500).json({
-                  error: err
-                });
-              });
-          }
-        });
-      }
+          });
+
+      });
+
     });
 });
 
 // @route   POST api/user
 // @desc    Sign in an existing user
 router.post("/signin", (req, res) => {
+
   User.find({ email: req.body.email })
     .exec()
     .then(users => {
+
       if (users.length < 1) {
         return res.status(401).json({
           message: "Incorrect Login Details!"
         });
       }
+
       bcrypt.compare(req.body.password, users[0].password, (err, found) => {
+
         if (err) {
           return res.status(401).json({
             message: "Incorrect Login Details!"
           });
         }
+
         if (found) {
           return res.status(200).json({
             userId: users[0]._id,
             message: "Login successful!"
           });
         }
+
         res.status(401).json({
           message: "Incorrect Login Details!"
         });
@@ -86,9 +98,17 @@ router.post("/signin", (req, res) => {
 // @route   GET api/user/userId
 // @desc    Get an existing user by user id
 router.get("/:userId", (req, res) => {
+  
   User.findById(req.params.userId)
     .exec()
     .then(foundUser => {
+
+      if (!foundUser) {
+        return res.status(401).json({
+          message: "Can't find user!"
+        });
+      }
+
       res.status(200).json(foundUser);
     })
     .catch(err => {
@@ -102,9 +122,17 @@ router.get("/:userId", (req, res) => {
 // @route   PUT api/user
 // @desc    Update an existing user
 router.put("/", (req, res) => {
+  
   User.findById(req.body._id)
     .exec()
     .then(foundUser => {
+
+      if (!foundUser) {
+        return res.status(401).json({
+          message: "Can't find user!"
+        });
+      }
+
       foundUser.teams = req.body.teams;
       foundUser.save().then(() => {
         res.status(201).json({
